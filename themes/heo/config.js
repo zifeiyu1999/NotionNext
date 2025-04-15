6713519364d5b77e54a3e49c928c8cc252dd0d65
zifeiyu1999 <66876278+zifeiyu1999@users.noreply.github.com>
2025-04-15 15:39:29 +0800

    Update config.js

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -118,7 +118,7 @@ const CONFIG = {
   HEO_SOCIAL_CARD_TITLE_1: '交流频道',
   HEO_SOCIAL_CARD_TITLE_2: '加入我们的社群讨论分享',
   HEO_SOCIAL_CARD_TITLE_3: '点击加入社群',
-  HEO_SOCIAL_CARD_URL: 'https://blog.341359.xyz',
+  HEO_SOCIAL_CARD_URL: 'https://t.me/shiyu_ai',
 
   // 底部统计面板文案
   HEO_POST_COUNT_TITLE: '文章数:',
